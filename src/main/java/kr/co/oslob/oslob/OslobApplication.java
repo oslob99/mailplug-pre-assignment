e1baa732a075741e4af18b105f1ecf7e479bf8f7
oslob99 <jkh991116@naver.com>
2023-08-08 14:14:24 +0900

    Initial commit

---
+++ src/main/java/kr/co/oslob/oslob/OslobApplication.java
@@ -0,0 +1,13 @@
+package kr.co.oslob.oslob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OslobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OslobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/oslob/oslob/OslobApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.oslob.oslob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OslobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
